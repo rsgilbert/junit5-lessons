b3022bb826946506e9ed58009253983a523393ca
Gilbert <ssenyonjogilbert@gmail.com>
2021-06-05 13:47:27 +0300

    Performed group test with assertAll

---
+++ src/mvn-module/src/test/java/StandardAssertionsTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StandardAssertionsTest {
+
+
+    @Test
+    @DisplayName("Test assertions")
+    void standardAssertions() {
+        assertEquals(2, 2);
+        assertTrue(true, "This is assertion is true");
+        assertFalse(false, () -> "Lambda for long messages");
+
+    }
+
+    @Test
+    @DisplayName("Grouped test with assertAll")
+    void groupedAssertions() {
+        String name = "John Lugalu";
+        // All assertions are executed
+        // Any failures will be reported together
+        assertAll("Multiple assertions on name",
+                () -> assertEquals("John Lugalu", name, "Name not matching"),
+                () -> assertEquals(name.length(), 11),
+                () -> assertFalse(name.isEmpty(), "Name can not be empty")
+        );
+    }
+}
