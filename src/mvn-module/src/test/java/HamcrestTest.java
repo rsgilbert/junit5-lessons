ad89ad33aa6f6cd59f4fbd27ab5f66caac3853d5
Gilbert <ssenyonjogilbert@gmail.com>
2021-06-05 16:59:33 +0300

    Used hamcrest matchers and assertThat MatcherAssert

---
+++ src/mvn-module/src/test/java/HamcrestTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+
+public class HamcrestTest {
+
+    @Test
+    @DisplayName("Hamcrest assertThat test")
+    void assertWithHamcrestMatcherTest() {
+        assertThat(2 + 1, equalTo(3));
+        assertThat("Three", containsString("ee"));
+        assertThat("Hi you", notNullValue());
+    }
+}
