a66d435b655ac0d512ed7e9eab6bc5eed42e7694
Gilbert <ssenyonjogilbert@gmail.com>
2021-06-06 10:17:58 +0300

    Implemented SimpleMultipleInputTest for testing the same logic on multiple inputs t

---
+++ src/mvn-module/src/test/java/stdtests/SimpleMultipleInputTest.java
@@ -0,0 +1,17 @@
+package stdtests;
+
+import org.junit.Test;
+
+public class SimpleMultipleInputTest {
+    // Loop through all the inputs exercising the same test logic
+    @Test
+    public void simpleTest() {
+        String[] input = { "A", "B", "C"};
+        for(String s: input)
+            exercise(s);
+    }
+
+    private void exercise(String s) {
+        System.out.println(s);
+    }
+}
