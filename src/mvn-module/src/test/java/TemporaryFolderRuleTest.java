25b87448ab9e12109f3ee494fe655abfd50def82
Gilbert <ssenyonjogilbert@gmail.com>
2021-06-06 07:10:23 +0300

    Implemented TemporaryFolderRuleTest after adding support for JUnit4 to Junit jupiter migration in the pom.xml file

---
+++ src/mvn-module/src/test/java/TemporaryFolderRuleTest.java
@@ -0,0 +1,35 @@
+import org.junit.Rule;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.migrationsupport.rules.EnableRuleMigrationSupport;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.IOException;
+
+/**
+ * Uses a JUnit4 TemporaryFolder rule within a Jupiter test
+ */
+@EnableRuleMigrationSupport
+public class TemporaryFolderRuleTest {
+
+    @Rule
+    TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @BeforeEach
+    void setup() throws IOException {
+        temporaryFolder.create();
+    }
+
+
+    @Test
+    void test() {
+        System.out.println("Temporary folder path: " + temporaryFolder.getRoot());
+    }
+
+    @AfterEach
+    void tearDown() {
+        temporaryFolder.delete();
+    }
+}
